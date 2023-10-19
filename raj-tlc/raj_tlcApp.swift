7378a91785c80fa6f533e563c9039f65dacf2ad6
anbu <anbu@thelaunchclub.in>
2023-10-19 13:51:48 +0530

    Initial Commit

---
+++ raj-tlc/raj_tlcApp.swift
@@ -0,0 +1,32 @@
+//
+//  raj_tlcApp.swift
+//  raj-tlc
+//
+//  Created by Anbu p on 19/10/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct raj_tlcApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
